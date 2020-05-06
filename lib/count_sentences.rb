2aaa3a533d442786e790b69c5ef806083ce1ff96
Shiyun Lu <lushiyun@outlook.com>
2020-05-06 01:23:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    str_arr = self.split(" ")
+    binding.pry
+    num = 0
+    str_arr.each do |word|
+      num += 1 if word.sentence? || word.question? || word.exclamation?
+    end
+    num
   end
 end
