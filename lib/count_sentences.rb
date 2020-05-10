a20ac207b349a5a19f73c551727256675882ca67
claudia v <claudia4ro@yahoo.com>
2020-05-10 17:02:25 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,48 @@
 require 'pry'
 
-class String
 
+class String
+  
+  
   def sentence?
     
+   if self.end_with?(".") 
+     return true 
+   else
+     false
+   end
   end
-
+  
+  
   def question?
+    if self.end_with?("?") 
+     return true 
+   else
+     false
+   end
 
   end
 
   def exclamation?
+    if self.end_with?("!") 
+     return true 
+   else
+     false
+   end
 
   end
 
   def count_sentences
+    array = self.split(/[?!.]/)
+    count = 0 
+    array.each do |word|
+      if word.length != 0 
+        count += 1 
+      end
+    end
+    count
+      
+    
 
   end
 end
