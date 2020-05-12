0343ca7242b2f4106ceae9690b6134c1e27bcb66
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-12 14:32:31 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split(" ").any?{|words| words.end_with?(".")}
   end
 
   def question?
-
+    self.split(" ").any? {|words| words.end_with?("?")}
   end
 
   def exclamation?
-
+    self.split(" ").any? {|words| words.end_with?("!")}
   end
 
   def count_sentences
-
+    self.split(" ").collect.count do |words|
+      words.end_with?(".", "!", "?")
+    end 
   end
 end
